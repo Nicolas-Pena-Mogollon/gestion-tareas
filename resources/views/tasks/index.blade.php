3c1a05c14ee1cc963c61e0dcc04640230a4af71a
Nicolás Peña Mogollón <nicop4643@gmail.com>
2024-06-14 05:31:13 -0500

    resolviendo error de bootstrap

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -73,8 +73,5 @@
     };
 
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
 </script>
 @endsection
